201f8941e7a81ce28373dfa5a1db53a57ed48233
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-07-05 16:20:07 +0100

    add conditional comment based on amount

--- src/App.js
+++ src/App.js
@@ -32,6 +32,72 @@ function App() {
         max={100}
         />
 
+        {value === 0 && (
+                <p>
+                Insert comment here
+              </p>
+        )}
+
+        {value === 10 && (
+                <p>
+                Insert comment here
+              </p>
+        )}
+
+        {value === 20 && (
+                <p>
+                Insert comment here
+              </p>
+        )}
+
+        {value === 30 && (
+                <p>
+                Insert comment here
+              </p>
+        )}
+
+        {value === 40  && (
+                <p>
+                Insert comment here
+              </p>
+        )}
+
+        {value === 50 && (
+                <p>
+                Insert comment here
+              </p>
+        )}
+
+        {value === 60 && (
+                <p>
+                Insert comment here
+              </p>
+        )}
+
+        {value === 70 && (
+                <p>
+                Insert comment here
+              </p>
+        )}
+
+        {value === 80 && (
+                <p>
+                Insert comment here
+              </p>
+        )}
+
+        {value === 90 && (
+                <p>
+                Insert comment here (90 percent!)
+              </p>
+        )}
+
+        {value === 100 && (
+                <p>
+                This is where the text goes based on your amount.
+              </p>
+        )}
+
     </div>
   );
 }
