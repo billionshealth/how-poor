c53ed0378e0b9c78957d450fd944a8a586cc5b17
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-07-05 15:53:25 +0100

    initial slider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Slider from '@mui/material/Slider';
+
 
 function App() {
+  const [sliderLocation, setSliderLocation] = useState(0);
+  const valuetext = 0;
+
+  // TODO: add conditional mapping based on slider value
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          How much of your net worth was in crypto?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Slider
+            aria-label="Temperature"
+            defaultValue={30}
+            getAriaValueText={valuetext}
+            valueLabelDisplay="auto"
+            step={10}
+            marks
+            min={10}
+            max={110}
+          />
+          <Slider defaultValue={30} step={10} marks min={10} max={110} disabled />
+
       </header>
     </div>
   );
